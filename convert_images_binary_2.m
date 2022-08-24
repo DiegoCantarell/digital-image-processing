2afa945cbd49ef1ff969e3a143a74eceacf5b3d6
Diego Cantarell <92350521+DiegoCantarell@users.noreply.github.com>
2022-08-24 11:55:02 -0500

    Adding convert_images_binary_2.m

---
+++ convert_images_binary_2.m
@@ -0,0 +1,25 @@
+% CONVERT IMAGES TO BINARY IMAGES example 2
+I = imread('pout.tif');
+J = imread('cameraman.tif');
+II = im2bw(I); %We convert the image to binary
+JJ = im2bw(J);
+[m1,n1]=size(I);
+[m2,n2]=size(J);
+IN=imresize(II,[min(m1,m2),min(n1,n1)]);
+JN=imresize(JJ,[min(m1,m2),min(n1,n1)]);
+Iand=and(IN,JN);
+Ior=or(IN,JN);
+Ixor=xor(IN,JN);
+Inot=not(IN);
+subplot(3,2,1)
+imshow(I);
+subplot(3,2,2)
+imshow(J)
+subplot(3,2,3)
+imshow(Iand)
+subplot(3,2,4)
+imshow(Ior)
+subplot(3,2,5)
+imshow(Ixor)
+subplot(3,2,6)
+imshow(Inot)
